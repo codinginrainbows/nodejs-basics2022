5d04db0627d13b647eb6bcd046b3c54e54b2c4f5
Gabriel Garcia <gabrielmgarcia96@gmail.com>
2022-03-28 16:59:31 -0300

    query params

--- src/index.js
+++ src/index.js
@@ -2,8 +2,50 @@ const express = require("express")
 
 const app = express()
 
-app.get("/", (request, response) => {
-    return response.json({ message: "hello friend" })
+app.get("/courses", (request, response) => {
+    const query = request.query
+    console.log(query)
+
+    return response.json([
+        "Course Node",
+        "Course React",
+        "Course React Native",
+    ])
+})
+
+app.post("/courses", (request, response) => {
+    return response.json([
+        "Course Node",
+        "Course React",
+        "Course React Native",
+        "Course PHP",
+    ])
+})
+
+app.put("/courses/:id", (request, response) => {
+    return response.json([
+        "Course Node",
+        "Course React",
+        "Course React Native",
+        "Course Laravel",
+    ])
+})
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json([
+        "Course Node",
+        "Course React.js",
+        "Course React Native",
+        "Course Laravel",
+    ])
+})
+
+app.delete("/courses/:id", (resquest, response) => {
+    return response.json([
+        "Course Node",
+        "Course React.js",
+        "Course React Native",
+    ])
 })
 
 app.listen(3333)
